4c2317034ce92b5624cc863c08658ded524b4c74
Roy Nouneh <transcript.jpg@gmail.com>
2021-10-19 15:04:54 -0400

    Some columns

--- classic/src/view/main/Tech.js
+++ classic/src/view/main/Tech.js
@@ -9,35 +9,79 @@ Ext.define('MyApp.view.main.Tech', {
         'Ext.layout.container.Fit'
     ],
 
-    xtype: 'layout-fit',
+//    xtype: 'layout-fit',
 
-    layout: 'fit',
-    width: 500,
-    height: 400,
+    layout: 'anchor',
+    width: 800,
+    height: 500,
     cls: Ext.baseCSSPrefix + 'shadow',
-
-    bodyPadding: 25,
+    bodyPadding: 4,
 
     items: {
-        /*
-        title: 'Inner Panel',
-        html: '<p>This panel is fit within its container.</p>',
-        */
-        bodyPadding: 15,
-        border: true,
-        items: [{
+        border: false,
+        cls: ['all-panels', 'panel-cyan'],
+        anchor: '100% 100%',
+        layout: 'anchor',
+        items: [
+        {
             xtype: 'panel',
-            border: true,
-            height: 95,
-            items: [{
+            border: false,
+            margin: 4,
+            cls: ['all-panels', 'panel-darkblue'],
+            layout: 'anchor',
+            anchor: '100% 15%',
+        },
+        {
+            xtype: 'panel',
+            border: false,
+            margin: 4,
+            cls: ['all-panels', 'panel-orange'],
+            layout: 'column',
+            anchor: '100% 86%',
+            items: [
+            {
                 xtype: 'panel',
-                border: true,
-                height: 45,
-                items: [{
+                border: false,
+                margin: 4,
+                cls: ['all-panels', 'panel-pink', 'col-height'],
+                columnWidth: 0.11,
+                height: 388,
+            },
+            {
+                xtype: 'panel',
+                border: false,
+                margin: 4,
+                cls: ['all-panels', 'panel-darkgrey', 'col-height'],
+                columnWidth: .89,
+                layout: 'anchor',
+                items:{
+                    //invisible anchor
                     xtype: 'panel',
-                    border: true,
-                    height: 15
-                }]
+                    border: false,
+                    margin: 0,
+                    layout: 'column',
+                    anchor: '100% 100%',
+                    width: 661,
+                    height: 378,
+                    items:[{
+                        xtype: 'panel',
+                        border: false,
+                        margin: 4,
+                        cls: ['all-panels', 'panel-blue', 'col-height'],
+                        columnWidth: 0.11,
+                    },
+                    {
+                        xtype: 'panel',
+                        border: false,
+                        margin: 4,
+                        cls: ['all-panels', 'panel-green', 'col-height'],
+                        layout: 'column',
+                        columnWidth: .89,
+                        items:[{
+
+                        }]
+                    }]
+                }
             }]
         }]
     }
